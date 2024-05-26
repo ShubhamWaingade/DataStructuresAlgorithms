be7e21514a2d354812aff6fa938a3cbd5c20faca
ShubhamWaingade <106223246+ShubhamWaingade@users.noreply.github.com>
2024-05-26 18:18:10 +0530

    Time: 55 ms (81.07%), Space: 17.7 MB (41.19%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map_index = {}
+        
+        for ind in range(len(nums)):
+            if map_index.get(target-nums[ind], -1) != -1:
+                return [ind, map_index[target-nums[ind]]]
+            else:
+                map_index[nums[ind]] = map_index.get(nums[ind], ind)
