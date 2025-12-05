1d0907edbfacd4f97e7db649b6851be196831dc8
ShubhamWaingade <106223246+ShubhamWaingade@users.noreply.github.com>
2025-12-05 17:04:06 +0530

    Git Check

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,5 +5,6 @@ class Solution:
         for ind in range(len(nums)):
             if map_index.get(target-nums[ind], -1) != -1:
                 return [ind, map_index[target-nums[ind]]]
+                
             else:
                 map_index[nums[ind]] = map_index.get(nums[ind], ind)
