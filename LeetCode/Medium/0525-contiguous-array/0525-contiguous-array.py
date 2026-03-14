50e7cbcc40115e74ce28e642c7da465e81c92e87
ShubhamWaingade <106223246+ShubhamWaingade@users.noreply.github.com>
2026-03-14 21:09:06 +0530

    0525-contiguous-array

--- LeetCode/Medium/0525-contiguous-array/0525-contiguous-array.py
+++ LeetCode/Medium/0525-contiguous-array/0525-contiguous-array.py
@@ -1,20 +1,41 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        max_len = 0
-        given_sum_k = 0
-        prefix_sum = 0
-        temp_hash = {}
+        """
+        111 00
+        How to know to shrink window or increase?
+        Have to split, thus recursive and worse than O(n^2)
 
-        new_nums = [-1 if i == 0 else i for i in nums]
+        Not every result starts at the beginning e.g. 111 00
 
-        for i in range(len(new_nums)):
-            prefix_sum += new_nums[i]
-            if prefix_sum == given_sum_k:
-                max_len = i + 1
-            
-            elif prefix_sum in temp_hash:
-                new_temp_len = i - temp_hash.get(prefix_sum)
-                max_len = max(max_len, new_temp_len)
+        Is it possible to know exactly where the first 1 was, and then virify after
+        count[1] == count[0]?
+
+        Map each index to pait (count[0], count[1])
+        Use the diff between the counts to lookup what we need
+        """
+        ans = 0
+        diff_index = {}
+        zero, one = 0, 0
+
+        for i, n in enumerate(nums):
+            if n == 0:
+                zero += 1
             else:
-                temp_hash[prefix_sum] = i
-        return max_len
+                one += 1
+            
+            if one - zero not in diff_index:
+                diff_index[one - zero] = i
+            
+            if one == zero:
+                ans = one + zero
+            else:
+                idx = diff_index[one - zero]
+                ans = max(ans, i - idx)
+        
+        return ans
+            
+
+        
+
+
+        
